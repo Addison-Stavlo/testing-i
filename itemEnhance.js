6f12f1ca6744bf88db20db4cfe7a84af518c02e6
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-29 16:41:00 -0500

    refactored item naming function

--- itemEnhance.js
+++ itemEnhance.js
@@ -2,33 +2,36 @@ module.exports = {
   success,
   failure
 };
+function nameItem(item) {
+  if (item.level <= 15) {
+    item.name = `[+${item.level}] ${item.baseName}`;
+  } else {
+    switch (item.level) {
+      case 16:
+        item.name = `[PRI] ${item.baseName}`;
+        break;
+      case 17:
+        item.name = `[DUO] ${item.baseName}`;
+        break;
+      case 18:
+        item.name = `[TRI] ${item.baseName}`;
+        break;
+      case 19:
+        item.name = `[TET] ${item.baseName}`;
+        break;
+      case 20:
+        item.name = `[PEN] ${item.baseName}`;
+        break;
+    }
+  }
+}
 
 function success(item) {
   if (item.level >= 20) {
     return item;
   } else {
     item.level += 1;
-    if (item.level <= 15) {
-      item.name = `[+${item.level}] ${item.baseName}`;
-    } else {
-      switch (item.level) {
-        case 16:
-          item.name = `[PRI] ${item.baseName}`;
-          break;
-        case 17:
-          item.name = `[DUO] ${item.baseName}`;
-          break;
-        case 18:
-          item.name = `[TRI] ${item.baseName}`;
-          break;
-        case 19:
-          item.name = `[TET] ${item.baseName}`;
-          break;
-        case 20:
-          item.name = `[PEN] ${item.baseName}`;
-          break;
-      }
-    }
+    nameItem(item);
     return item;
   }
 }
@@ -47,23 +50,7 @@ function failure(item) {
     item.durability = Math.max(0, item.durability - 10);
     if (item.level > 16) {
       item.level -= 1;
-      switch (item.level) {
-        case 16:
-          item.name = `[PRI] ${item.baseName}`;
-          break;
-        case 17:
-          item.name = `[DUO] ${item.baseName}`;
-          break;
-        case 18:
-          item.name = `[TRI] ${item.baseName}`;
-          break;
-        case 19:
-          item.name = `[TET] ${item.baseName}`;
-          break;
-        case 20:
-          item.name = `[PEN] ${item.baseName}`;
-          break;
-      }
+      nameItem(item);
     }
   }
   return item;
