2d5db5535ade42b4bdca7c2b071dc3abc3b19cca
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-29 16:38:16 -0500

    failure test suite added

--- itemEnhance.js
+++ itemEnhance.js
@@ -1,7 +1,6 @@
 module.exports = {
-  success
-  //   fail,
-  //   repair
+  success,
+  failure
 };
 
 function success(item) {
@@ -32,4 +31,40 @@ function success(item) {
     }
     return item;
   }
+}
+
+function failure(item) {
+  if (item.level <= 4) {
+    return success(item);
+  }
+  if (item.level <= 6 && item.type === "weapon") {
+    return success(item);
+  }
+
+  if (item.level <= 14) {
+    item.durability = Math.max(0, item.durability - 5);
+  } else {
+    item.durability = Math.max(0, item.durability - 10);
+    if (item.level > 16) {
+      item.level -= 1;
+      switch (item.level) {
+        case 16:
+          item.name = `[PRI] ${item.baseName}`;
+          break;
+        case 17:
+          item.name = `[DUO] ${item.baseName}`;
+          break;
+        case 18:
+          item.name = `[TRI] ${item.baseName}`;
+          break;
+        case 19:
+          item.name = `[TET] ${item.baseName}`;
+          break;
+        case 20:
+          item.name = `[PEN] ${item.baseName}`;
+          break;
+      }
+    }
+  }
+  return item;
 }
--- itemEnhance.spec.js
+++ itemEnhance.spec.js
@@ -8,15 +8,15 @@ describe("success() method", () => {
       baseName: "Elven Warblade",
       name: "Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 0 //starting, max 20
+      durability: 100,
+      level: 0
     };
     const expectedOutput = {
       baseName: "Elven Warblade",
       name: "[+1] Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 1 //starting, max 20
+      durability: 100,
+      level: 1
     };
     // execute SUT (system under test)
     const testedItem = itemEnhance.success(exampleWeapon);
@@ -31,15 +31,15 @@ describe("success() method", () => {
       baseName: "Elven Warblade",
       name: "[+15] Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 15 //starting, max 20
+      durability: 100,
+      level: 15
     };
     const expectedOutput = {
       baseName: "Elven Warblade",
       name: "[PRI] Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 16 //starting, max 20
+      durability: 100,
+      level: 16
     };
     //execute SUT
     const testedItem = itemEnhance.success(exampleWeapon);
@@ -53,19 +53,123 @@ describe("success() method", () => {
       baseName: "Elven Warblade",
       name: "[PEN] Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 20 //starting, max 20
+      durability: 100,
+      level: 20
     };
     const expectedOutput = {
       baseName: "Elven Warblade",
       name: "[PEN] Elven Warblade",
       type: "weapon",
-      durability: 100, //starting & max 100
-      level: 20 //starting, max 20
+      durability: 100,
+      level: 20
     };
     //execute SUT
     const testedItem = itemEnhance.success(exampleWeapon);
     //assert/compare
+    expect(testedItem).toEqual(expectedOutput);
+  });
+});
+
+describe("failure() method", () => {
+  test("level 4 armor cant fail", () => {
+    //setup
+    const exampleArmor = {
+      baseName: "Dwarven Bladestopper",
+      name: "[+4] Dwarven Bladestopper",
+      type: "armor",
+      durability: 100,
+      level: 4
+    };
+    const expectedOutput = {
+      baseName: "Dwarven Bladestopper",
+      name: "[+5] Dwarven Bladestopper",
+      type: "armor",
+      durability: 100,
+      level: 5
+    };
+    //execute SUT
+    const testedItem = itemEnhance.failure(exampleArmor);
+    //assert/compare
+    expect(testedItem).toEqual(expectedOutput);
+  });
+  test("level 6 weapon can't fail", () => {
+    const exampleWeapon = {
+      baseName: "Dwarven Axe",
+      name: "[+6] Dwarven Axe",
+      type: "weapon",
+      durability: 100,
+      level: 6
+    };
+    const expectedOutput = {
+      baseName: "Dwarven Axe",
+      name: "[+7] Dwarven Axe",
+      type: "weapon",
+      durability: 100,
+      level: 7
+    };
+    //execute SUT
+    const testedItem = itemEnhance.failure(exampleWeapon);
+    //assert/compare
+    expect(testedItem).toEqual(expectedOutput);
+  });
+  test("failure below level 15 loses 5 durability", () => {
+    const exampleWeapon = {
+      baseName: "Dwarven Axe",
+      name: "[+10] Dwarven Axe",
+      type: "weapon",
+      durability: 100,
+      level: 10
+    };
+    const expectedOutput = {
+      baseName: "Dwarven Axe",
+      name: "[+10] Dwarven Axe",
+      type: "weapon",
+      durability: 95,
+      level: 10
+    };
+    //execute SUT
+    const testedItem = itemEnhance.failure(exampleWeapon);
+    //assert
+    expect(testedItem).toEqual(expectedOutput);
+  });
+  test("failure above level 14 loses 10 durability", () => {
+    const exampleWeapon = {
+      baseName: "Dwarven Axe",
+      name: "[+15] Dwarven Axe",
+      type: "weapon",
+      durability: 100,
+      level: 15
+    };
+    const expectedOutput = {
+      baseName: "Dwarven Axe",
+      name: "[+15] Dwarven Axe",
+      type: "weapon",
+      durability: 90,
+      level: 15
+    };
+    //execute SUT
+    const testedItem = itemEnhance.failure(exampleWeapon);
+    //asser
+    expect(testedItem).toEqual(expectedOutput);
+  });
+  test("failure above level 16 loses a level", () => {
+    const exampleWeapon = {
+      baseName: "Dwarven Axe",
+      name: "[DUO] Dwarven Axe",
+      type: "weapon",
+      durability: 100,
+      level: 17
+    };
+    const expectedOutput = {
+      baseName: "Dwarven Axe",
+      name: "[PRI] Dwarven Axe",
+      type: "weapon",
+      durability: 90,
+      level: 16
+    };
+    //execute SUT
+    const testedItem = itemEnhance.failure(exampleWeapon);
+    //assert
     expect(testedItem).toEqual(expectedOutput);
   });
 });
